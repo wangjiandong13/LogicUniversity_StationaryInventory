4503d3dd0d99714ad292d8f9f1ea65001d10b42b
petersithu <peter.sithu.2011@gmail.com>
2015-09-01 18:15:15 +0800

    update by peter

--- BusinessLogic/AdjustVoucherController.cs
+++ BusinessLogic/AdjustVoucherController.cs
@@ -12,13 +12,42 @@ namespace BusinessLogic
     {
         StationeryInventory_Team_05Entities ctx = new StationeryInventory_Team_05Entities();
 
-        public List<AdjustmentVoucher> getAdjVoucherList(string adjId)
+        /// <summary>
+        /// Get Adjustment Voucher by Date or ID
+        /// </summary>
+        /// <param name="adjId">AdjustVoucher ID</param>
+        /// <param name="startDate">Date</param>
+        /// <param name="endDate">Date</param>
+        /// <returns></returns>
+        public List<AdjustmentVoucher> getAdjVoucher(string adjId,DateTime startDate,DateTime endDate)
         {
-            AdjustmentVoucher adjlist = new AdjustmentVoucher();
+            List<AdjustmentVoucher> adjustlist = new List<AdjustmentVoucher>();
+            
+            if(adjId == null && startDate == null && endDate == null)
+            {
+                adjustlist = (from x in ctx.AdjustmentVoucher
+                              select x).ToList();
+            }
+            else if(startDate == null && endDate == null)
+            {
+                adjustlist = (from x in ctx.AdjustmentVoucher
+                              where x.Date == startDate && x.Date == endDate
+                              select x).ToList();
+            }
+            else
+            {
+                adjustlist = (from x in ctx.AdjustmentVoucher
+                              where x.AdjID == adjId
+                              select x).ToList();
+            }
+             
 
-           // adjlist = from x in ctx.AdjustmentVoucher
+            return adjustlist;
+        }
 
-
+        public List<AdjustmentDetail> getAdjVoucherDetail(string adjId)
+        {
+            //var detail = from x in 
             return null;
         }
     }
