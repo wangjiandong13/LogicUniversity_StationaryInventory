bd1e2c9d55bf321e708b12a4344ddbd366a77012
a0134741 <a0134741@u.nus.edu>
2015-09-01 14:07:45 +0800

    Update

---
+++ BusinessLogic/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Employee
+    {
+        Model.StationeryInventory_Team_05Entities ctx = new Model.StationeryInventory_Team_05Entities();
+        /// <summary>
+        /// GetEmployeebyRole
+        /// </summary>
+        /// <param name="role">Role ID</param>
+        /// <returns></returns>
+        public List<Model.Employee> getemployeebyrole(string role)
+        {
+            var employees = from c in ctx.Employee
+                          where c.RoleID==role
+                          select c;
+            return employees.ToList();
+        }
+        /// <summary>
+        /// GetEmployeebyDeptName
+        /// </summary>
+        /// <param name="DeptName">Department name</param>
+        /// <returns></returns>
+        public List<Model.Employee> getemployeebyDeptName(string DeptName)
+        {
+            var employees = from c in ctx.Employee
+                            where c.DeptID==DeptName
+                            select c;
+            return employees.ToList();
+        }
+    }
+}
