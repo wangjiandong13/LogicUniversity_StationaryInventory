209db8580e9ca22dfe8b80d9dbc9b246f46f67a9
a0070007 <a0070007@u.nus.edu>
2015-09-01 18:13:04 +0800

    Update by esther

---
+++ Model/ProposePo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using TrackableEntities;
+
+namespace Model
+{
+    [JsonObject(IsReference = true)]
+    [DataContract]
+    class ProposePo
+    {
+        public ProposePo() { }
+
+        [DataMember]
+        public string ItemID { get; set; }
+        [DataMember]
+        public string ItemName { get; set; }
+        [DataMember]
+        public int totalQty { get; set; }
+        [DataMember]
+        public int supplier1Qty { get; set; }
+        [DataMember]
+        public int supplier2Qty { get; set; }
+        [DataMember]
+        public int supplier3Qty { get; set; }
+
+    }
+}
