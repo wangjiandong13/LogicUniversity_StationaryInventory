5c8aaf0835cfca5ea0e940e4b2d40b06550278b6
PhuPwintWai <phupwintwai269@gmail.com>
2015-09-02 16:22:22 +0800

    Update By Apple

---
+++ BusinessLogic/CollectionPointController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class CollectionPointController
+    {
+        Model.StationeryInventory_Team_05Entities ctx = new Model.StationeryInventory_Team_05Entities();
+
+        /// <summary>
+        /// I think no need 
+        /// </summary>
+        /// <returns></returns>
+        public List<Model.CollectionPoint> getCollectionPoint()
+        {
+            var cp = from c in ctx.CollectionPoint
+                     select c;
+
+            return cp.ToList();
+        }
+
+        /// <summary>
+        /// get collection point according to Dept ID
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public List<Model.CollectionPoint> getCollectionPoint(Model.Department d)
+        {
+            var cp = from c in ctx.CollectionPoint
+                     where c.CPID == d.CPID
+                     select c;
+
+            return cp.ToList();
+        }
+    }
+}
