db5af4d2158346018c0e9338739550678af23c30
a0070007 <a0070007@u.nus.edu>
2015-09-09 18:47:23 +0800

    Add Notification API

---
+++ RestService/NotificationAPI.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Model;
+
+namespace RestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "NotificationAPI" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select NotificationAPI.svc or NotificationAPI.svc.cs at the Solution Explorer and start debugging.
+    public class NotificationAPI : INotificationAPI
+    {
+        public bool createNotification(Notification notification)
+        {
+            BusinessLogic.NotificationController BL = new BusinessLogic.NotificationController();
+            return BL.createNotification(notification);
+        }
+
+        public bool changeStatusToRead(string NotifID)
+        {
+            BusinessLogic.NotificationController BL = new BusinessLogic.NotificationController();
+            return BL.changeStatusToRead(NotifID);
+        }
+
+        public List<Notification> getNotification(string EmpID)
+        {
+            BusinessLogic.NotificationController BL = new BusinessLogic.NotificationController();
+            return BL.getNotification(EmpID);
+        }
+    }
+}
---
+++ Web/Service.js
@@ -0,0 +1,42 @@
+﻿var myapp = angular.module("_", []);
+
+var baseurl = "http://www.team5.com/api";
+
+myapp.service("service", ["$http", "$rootScope", service]);
+
+function service($http, $rootScope) {
+    this.getRequisitionList = function () {
+        $http.get(baseurl + "/requisitionAPI.svc/getRequisition/null/null/" + $rootScope.EmpID)
+        .success(function (result) {
+            $rootScope.$broadcast("RequisitionListLoaded", result);
+        })
+        .fail()
+    }
+
+    this.getCatalogList = function () {
+        $http.get(baseurl + "inventoryAPI.svc/getItem")
+        .success(function (result) {
+            $rootScope.$broadcast("CatalogListLoaded", result);
+        })
+        .fail()
+    }
+
+    this.getDepartment = function () {
+        $http.get(baseurl + "departmentAPI.svc/getDeptByID/" + $rootScope.DeptID)
+        .success(function (result) {
+            $rootScope.$broadcast("CatalogListLoaded", result);
+        })
+        .fail()
+    }
+
+    this.getNotificationList = function () {
+        $http.get(baseurl + "departmentAPI.svc/getDeptByID/" + $rootScope.DeptID)
+        .success(function (result) {
+            $rootScope.$broadcast("CatalogListLoaded", result);
+        })
+        .fail()
+    }
+
+
+
+}
