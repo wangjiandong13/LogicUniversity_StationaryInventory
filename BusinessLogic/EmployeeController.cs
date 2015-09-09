fdc8c9aa141c603a25597359ac3d95bb685c4732
a0134741 <a0134741@u.nus.edu>
2015-09-09 12:05:43 +0800

    Update

--- BusinessLogic/EmployeeController.cs
+++ BusinessLogic/EmployeeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -74,15 +75,46 @@ namespace BusinessLogic
                             where c.EmpID==e.EmpID
                             select c;
             Employee re = employees.FirstOrDefault();
+            
             if (re != null)
             {
-                if (re.Password == e.Password)
+                using (MD5 md5Hash = MD5.Create())
                 {
-                    return re;
+                    string source = e.Password + re.EmpID;
+                    string hash = GetMd5Hash(md5Hash, source);
+
+                    if (hash==re.Password)
+                    {
+                        return re;
+                    }
+                    else
+                    {
+                        return null;
+                    }
                 }
-                return null;
             }
             return null;
         }
+
+        public string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
     }
 }
