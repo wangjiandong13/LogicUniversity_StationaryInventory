dea8b1564a6443d5d3e93dacf2cd752861828c1f
a0070007 <a0070007@u.nus.edu>
2015-09-09 20:12:59 +0800

    update service

--- Web/Service.js
+++ Web/Service.js
@@ -5,6 +5,8 @@ var baseurl = "http://www.team5.com/api";
 myapp.service("service", ["$http", "$rootScope", service]);
 
 function service($http, $rootScope) {
+
+    //requisition.html
     //load requisition List 
     this.getRequisitionList = function () {
         $http.get(baseurl + "/requisitionAPI.svc/getRequisition/null/null/" + $rootScope.EmpID)
@@ -13,6 +15,72 @@ function service($http, $rootScope) {
         })
         .fail()
     }
+    //load requisition status for combobox binding
+    this.getRequisitionStatus = function () {
+        $http.get(baseurl + "/requisitionAPI.svc/getStatus")
+        .success(function (result) {
+            $rootScope.$broadcast("RequisitionStatusLoaded", result);
+        })
+        .fail()
+    }
+    //load Requisition for Search Request by ReqID OR load requisitionDetail.html
+    this.getRequisition = function () {
+        $http.get(baseurl + "/requisitionAPI.svc/getRequisitionById/" + $rootScope.RetID)
+        .success(function (result) {
+            $rootScope.$broadcast("RequisitionLoaded", result);
+        })
+        .fail()
+    }
+
+
+    //requisitionDetail.html
+    //load requisition detail list
+    this.getRequisitionDetailList = function () {
+        $http.get(baseurl + "requisitionAPI.svc/getRequisitionDetail/" + $rootScope.ReqID)
+        .success(function (result) {
+            $rootScope.$broadcast("RequisitionDetailListLoaded", result);
+        })
+        .fail()
+    }
+
+
+    //requestCart.html
+    //load request cart items
+    this.getRequestCart = function () {
+        $http.get(baseurl + "requestcartAPI.svc/getItems/" + $rootScope.EmpID)
+        .success(function (result) {
+            $rootScope.$broadcast("RequestCartLoaded", result);
+        })
+        .fail()
+    }
+    //remove item from request cart //POST
+    this.removeRequestCart = function () {
+        $http.post(baseurl + "requestcartAPI.svc/deleteItem", $rootScope.data)
+        .success(function (result) {
+            $rootScope.$broadcast("ItemRemoved", result);
+        })
+        .fail()
+    }
+    //update request cart item qty //POST
+    this.updateItem = function () {
+        $http.post(baseurl + "requestcartAPI.svc/updateItem", $rootScope.data)
+        .success(function (result) {
+            $rootScope.$broadcast("ItemUpdated", result);
+        })
+        .fail()
+    }
+    //create requisition //POST
+    this.createRequisition = function () {
+        $http.post(baseurl + "requestcartAPI.svc/createRequisition", $rootScope.data)
+        .success(function (result) {
+            $rootScope.$broadcast("RequisitionCreated", result);
+        })
+        .fail()
+    }
+
+
+    
+
 
     //load catalog list
     this.getCatalogList = function () {
@@ -50,23 +118,7 @@ function service($http, $rootScope) {
         .fail()
     }
 
-    //load request cart item list
-    this.getRequestCart = function () {
-        $http.get(baseurl + "requestcartAPI.svc/getItems/" + $rootScope.EmpID)
-        .success(function (result) {
-            $rootScope.$broadcast("RequestCartLoaded", result);
-        })
-        .fail()
-    }
-
-    //load requisition detail
-    this.getRequisitionDetail = function () {
-        $http.get(baseurl + "requisitionAPI.svc/getRequisitionDetail/" + $rootScope.ReqID)
-        .success(function (result) {
-            $rootScope.$broadcast("RequisitionDetailLoaded", result);
-        })
-        .fail()
-    }
+    
 
 
 
