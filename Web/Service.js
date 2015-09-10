ed2cf1f281d876436c22e2b7c7e3e57be74802b4
a0070007 <a0070007@u.nus.edu>
2015-09-10 09:55:53 +0800

    update service

--- Web/Service.js
+++ Web/Service.js
@@ -23,7 +23,7 @@ function service($http, $rootScope) {
         })
         .fail()
     }
-    //load Requisition for Search Request by ReqID OR load requisitionDetail.html
+    //load Requisition for Search Request by ReqID OR load requisitionDetail.html OR load requisitionApproval.html
     this.getRequisition = function () {
         $http.get(baseurl + "/requisitionAPI.svc/getRequisitionById/" + $rootScope.RetID)
         .success(function (result) {
@@ -34,11 +34,19 @@ function service($http, $rootScope) {
 
 
     //requisitionDetail.html
-    //load requisition detail list
+    //load requisition detail list OR load requisitionApproval.html
     this.getRequisitionDetailList = function () {
         $http.get(baseurl + "requisitionAPI.svc/getRequisitionDetail/" + $rootScope.ReqID)
         .success(function (result) {
             $rootScope.$broadcast("RequisitionDetailListLoaded", result);
+        })
+        .fail()
+    }
+    //Cancel Request
+    this.getRequisitionCancel = function () {
+        $http.get(baseurl + "requisitionAPI.svc/deleteRequisition/" + $rootScope.ReqID)
+        .success(function (result) {
+            $rootScope.$broadcast("RequisitionCancelled", result);
         })
         .fail()
     }
@@ -78,11 +86,9 @@ function service($http, $rootScope) {
         .fail()
     }
 
-
     
-
-
-    //load catalog list
+    //catalogList.hmtl OR catalogTile.html
+    //load catalog 
     this.getCatalogList = function () {
         $http.get(baseurl + "inventoryAPI.svc/getItem")
         .success(function (result) {
@@ -90,7 +96,33 @@ function service($http, $rootScope) {
         })
         .fail()
     }
+    //load category bind to combobox
+    this.getCategory = function () {
+        $http.get(baseurl + "catalogAPI.svc/getCategory")
+        .success(function (result) {
+            $rootScope.$broadcast("CategoryLoaded", result);
+        })
+        .fail()
+    }
+    //Search Item Name
+    this.getItemByName = function () {
+        $http.get(baseurl + "catalogAPI.svc/getItemByName/" + $rootScope.ItemName)
+        .success(function (result) {
+            $rootScope.$broadcast("ItemByNameLoaded", result);
+        })
+        .fail()
+    }
+    //Add to request cart
+    this.addItemToCart = function () {
+        $http.post(baseurl + "requestcartAPI.svc/addItem", $rootScope.data)
+        .success(function (result) {
+            $rootScope.$broadcast("ItemAddedToCart", result);
+        })
+        .fail()
+    }
 
+
+    //department.html
     //load department (one) page
     this.getDepartment = function () {
         $http.get(baseurl + "departmentAPI.svc/getDeptByID/" + $rootScope.DeptID)
@@ -99,7 +131,14 @@ function service($http, $rootScope) {
         })
         .fail()
     }
-
+    //Edit department //POST
+    this.updateDepartment = function () {
+        $http.post(baseurl + "departmentAPI.svc/updateDept/" + $rootScope.data)
+        .success(function (result) {
+            $rootScope.$broadcast("DepartmentUpdated", result);
+        })
+        .fail()
+    }
     //load department (list) page
     this.getDepartmentList = function () {
         $http.get(baseurl + "departmentAPI.svc/getAllDepartment")
@@ -109,6 +148,57 @@ function service($http, $rootScope) {
         .fail()
     }
 
+    //approval.html OR DH's requisition.html
+    //load employee bind to combobox
+    this.getDeptEmployee = function () {
+        $http.get(baseurl + "employeeAPI.svc/getemployeebyDeptID/" + $rootScope.DeptID)
+        .success(function (result) {
+            $rootScope.$broadcast("DeptEmployeeLoaded", result);
+        })
+        .fail()
+    }
+    //load requisition list pending approval
+    this.getRequisitionApprovalList = function () {
+        $http.get(baseurl + "requisitionAPI.svc/getRequisition/1/null/null")
+        .success(function (result) {
+            $rootScope.$broadcast("RequisitionApprovalListLoaded", result);
+        })
+        .fail()
+    }
+    //load requisition list by employee name (EmpID)
+    this.getRequisitionByEmpName = function () {
+        $http.get(baseurl + "requisitionAPI.svc/getRequisition/null/null/" + $rootScope.EmpID)
+        .success(function (result) {
+            $rootScope.$broadcast("RequisitionByNameLoaded", result);
+        })
+        .fail()
+    }
+
+
+    //requisitionApproval.html
+    //approve requisition
+    this.approveRequisition = function () {
+        $http.get(baseurl + "requisitionAPI.svc/approve/" + $rootScope.ReqId + "/" + $rootScope.HandledBy + "/"+ $rootScope.Remark)
+        .success(function (result) {
+            $rootScope.$broadcast("RequisitionApproved", result);
+        })
+        .fail()
+    }
+    //reject requisition
+    this.rejectRequisition = function () {
+        $http.get(baseurl + "requisitionAPI.svc/reject/" + $rootScope.ReqId + "/" + $rootScope.HandledBy + "/" + $rootScope.Remark)
+        .success(function (result) {
+            $rootScope.$broadcast("RequisitionRejected", result);
+        })
+        .fail()
+    }
+
+
+    //disbursement.html
+
+
+
+    //notification.html
     //load notification list
     this.getNotificationList = function () {
         $http.get(baseurl + "notificationAPI.svc/getnotification/" + $rootScope.EmpID)
