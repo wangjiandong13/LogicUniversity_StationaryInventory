a97b34fc264f49f4c7654424f8becee3bbfeb48c
a0134741 <a0134741@u.nus.edu>
2015-09-11 23:18:00 +0800

    Update

--- Web/js/controller/CatalogListController.js
+++ Web/js/controller/CatalogListController.js
@@ -2,7 +2,11 @@
     app.controller('catalogListCtrl', ['$rootScope', '$scope', 'BaseService', catalogListCtrl]);
     app.controller('catalogListDataCtrl', ['$rootScope', 'BaseService', catalogListDataCtrl]);
     function catalogListCtrl($rootScope, $scope, BaseService) {
+        //from session get empID
+        var EmpID = 11233;
+
         //get categoryselectData
+        console.log("enter catalogListCtrl");
         $scope.CategorySelectData = {
             availableOptions: [],
             selectedOption: { ItemCatID: 0, ItemDescription: 'ALL' }
@@ -24,13 +28,25 @@
             }
             //BaseService.
         }
-
+        $scope.add = function (item) {
+            var msg = '{"EmpID":' + EmpID + ',"ItemID":"' + item.ItemID + '","Qty":' + item.qty + '}';
+            console.log(msg);
+            BaseService.addItemToCart(msg)
+                .then(function (data) {
+                    alert("sucess!");
+                })
+        }
     }
     //load catalogList
     function catalogListDataCtrl($rootScope, BaseService) {
+        console.log("enter");
         BaseService.getCatalogList()
             .then(function (data) {
+                console.log(data);
                 $rootScope.catalogListdata = data;
+                $.each($rootScope.catalogListdata, function (index, value) {
+                    value.qty = 1;
+                });
             }, function (data) {
                 alert(data);
             }
