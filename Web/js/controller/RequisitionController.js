3a587215283de88ddddfce11a3b044faee3f7634
a0134741 <a0134741@u.nus.edu>
2015-09-11 14:38:28 +0800

    Update

--- Web/js/controller/RequisitionController.js
+++ Web/js/controller/RequisitionController.js
@@ -6,6 +6,20 @@
         //$rootScope.pageTitle = $route.current.title;
         $scope.viewCart = function () {
             location.href = '#/requestCart';
+        };
+        $scope.search = function () {
+            var status = $rootScope.optiondata.selectedOption.StatusID;
+            if (status == 0) { status = "null"; }
+            var ReqID = $scope.ReuisitionNo;
+            if (ReqID == null) { ReqID = "null" }
+            BaseService.getRequisitionList(status, ReqID, 11233)
+                .then(function (data) {
+                    console.log(data);
+                    $rootScope.Requisitions = data;
+                }, function (data) {
+                    alert(data);
+                }
+                )
         }
     }
     function RequisitionList($rootScope, BaseService) {
@@ -27,7 +41,7 @@
                 $rootScope.optiondata.availableOptions = data;
                 console.log(data);
                 $rootScope.optiondata.availableOptions.unshift({ StatusID: 0, StatusName: 'ALL' });
-            },function(data){
+            }, function (data) {
                 alert(data);
             })
     }
