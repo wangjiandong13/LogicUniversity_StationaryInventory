588870413d912ae59d244a462db812e7869b69d8
a0134741 <a0134741@u.nus.edu>
2015-09-11 16:28:32 +0800

    Update

--- Web/js/controller/requestCartController.js
+++ Web/js/controller/requestCartController.js
@@ -1,11 +1,13 @@
 ﻿define(['app'], function (app) {
     app.controller('RequestCartControllers', ['$rootScope', '$scope', 'BaseService', RequestCartControllers]);
-    //app.controller('requestCartListCtrl', ['$scope', '$rootScope', 'BaseService', requestCartListCtrl]);
+    app.controller('RequestCartListCtrl', ['$Scope', '$rootScope', 'BaseService', RequestCartList]);
 
     function RequestCartControllers(){
         console.log("enter  RequestCartControllers")
     }
-    function requestCartListCtrl($rootScope,BaseService) {
+    function RequestCartList($Scope, $rootScope, BaseService) {
+        console.log("enter requestCartListCtrls");
+        console.log(BaseService);
         BaseService.getRequestCart("11233")
             .then(function (data) {
                 console.log(data);
@@ -13,5 +15,8 @@
             }, function (data) {
                 alert(data);
             })
+        $Scope.delect = function (ItemID) {
+
+        }
     }
 })
