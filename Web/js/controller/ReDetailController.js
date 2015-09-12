ca67a6f5284385b17eb71ca7e5b1384f0e1101f3
a0134741 <a0134741@u.nus.edu>
2015-09-12 19:20:54 +0800

    Updat

--- Web/js/controller/ReDetailController.js
+++ Web/js/controller/ReDetailController.js
@@ -3,6 +3,61 @@
 
     function ReDetailController($scope, $rootScope, $routeParams, BaseService) {
         var reqid = $routeParams.reqid;
-        BaseService.g
+        $scope.reqid = reqid;
+        var myBaseService = BaseService;
+        BaseService.getRequisitionList("null", reqid, "null")
+            .then(function (data) {
+                console.log("getRequisitionList");
+                console.log(data);
+                $scope.RequisitionData = data;
+                myBaseService.getEmployee(data.EmpID)
+                       .then(function (data) {
+                           console.log("getEmployee");
+                           console.log(data);
+                           $scope.RequisitionData.EmpName = data.EmpName;
+                       })
+                if (data.HandledBy != null) {
+                    myBaseService.getEmployee(data.HandledBy)
+                           .then(function (data) {
+                               $scope.RequisitionData.HandledByName = data.EmpName;
+                           })
+                } else {
+                    data.HandledByName = "";
+                }
+                if (data.RetID != null) {
+                    myBaseService.getRetrievalListBySC("null","null",data.RetID)
+                           .then(function (data) {
+                               myBaseService.getEmployee(data.EmpID)
+                                   .then(function (data) {
+                                       $scope.RequisitionData.ProcessedByName = data.EmpName;
+                                   })
+                           })
+                } else {
+                    data.ProcessedByName = "";
+                }
+                
+            }, function (data) {
+                alert(data);
+            }
+            )
+        BaseService.getRequisitionDetailList(reqid)
+            .then(function (data) {
+                console.log(data);
+                $scope.RequisitionDetailLists = data;
+                $.each($scope.RequisitionDetailLists, function (index, value) {
+                    myBaseService.getItemDetail(value.ItemID)
+                        .then(function (data) {
+                            value.Description = data.ItemName;
+                            value.Measurement = data.UOM;
+                        }, function (data) {
+                            alert(data);
+                        }
+                        )
+                });
+                myBaseService.getItemDetail()
+            }, function (data) {
+                alert(data);
+            }
+            )
     }
 })
