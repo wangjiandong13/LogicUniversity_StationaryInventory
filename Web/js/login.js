6e8e15892b34df8622737e49fdaef75b4c480804
a0134741 <a0134741@u.nus.edu>
2015-09-13 20:25:38 +0800

    UPdae

--- Web/js/login.js
+++ Web/js/login.js
@@ -18,8 +18,12 @@ function logincontroller($scope, $http, md5) {
         console.log(jsonmsg);
         $http.post("http://www.team5.com/API/employeeAPI.svc/login", jsonmsg)
             .success(function (data) {
-                alert("sucess!")
+                alert("sucess!");
                 console.log(data);
+                $.session.set('EmpID', data.EmpID);
+                $.session.set('EmpName', data.EmpName);
+                $.session.set('RoleID', data.RoleID);
+                $.session.set('DeptID', data.DeptID);
             })
             .error(function () {
                 $scope.loginerrormessage = true;
