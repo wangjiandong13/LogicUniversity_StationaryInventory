190589e65294a850a76b49d1edf5ab9e10f05102
a0134741 <a0134741@u.nus.edu>
2015-09-13 20:33:45 +0800

    Updae

--- Web/js/login.js
+++ Web/js/login.js
@@ -1,36 +1,37 @@
 ﻿var loginApp = angular.module('loginAPP', ['angular-md5']);
 
-loginApp.controller('logincontrollers', ['$scope','$http', 'md5', logincontroller]);
-function logincontroller($scope, $http, md5) {
+loginApp.controller('logincontrollers', ['$scope', '$http', '$window', 'md5', logincontroller]);
+function logincontroller($scope, $http, $window, md5) {
     $scope.empID = "";
     $scope.password = "";
     $scope.message = false;
     $scope.loginclick = function () {
         $scope.message = false;
-        if ($scope.empID!=""&&$scope.password!=""){
-        var msg = {
-            EmpID: $scope.empID,
-            Password:""
-        }
-        msg.Password = md5.createHash($scope.password);
+        if ($scope.empID != "" && $scope.password != "") {
+            var msg = {
+                EmpID: $scope.empID,
+                Password: ""
+            }
+            msg.Password = md5.createHash($scope.password);
+
+            jsonmsg = angular.toJson(msg);
+            console.log(jsonmsg);
+            $http.post("http://www.team5.com/API/employeeAPI.svc/login", jsonmsg)
+                .success(function (data) {
+                    alert("sucess!");
+                    console.log(data);
+                    $window.sessionStorage.setItem('EmpID', data.EmpID);
+                    $window.sessionStorage.setItem('EmpName',data.EmpName);
+                    $window.sessionStorage.setItem('RoleID', data.RoleID);
+                    $window.sessionStorage.setItem('DeptID',  data.DeptID);
+                })
+                .error(function () {
+                    $scope.loginerrormessage = true;
+                })
+            }
+            else {
+                $scope.message = true;
+            }
         
-        jsonmsg = angular.toJson(msg);
-        console.log(jsonmsg);
-        $http.post("http://www.team5.com/API/employeeAPI.svc/login", jsonmsg)
-            .success(function (data) {
-                alert("sucess!");
-                console.log(data);
-                $.session.set('EmpID', data.EmpID);
-                $.session.set('EmpName', data.EmpName);
-                $.session.set('RoleID', data.RoleID);
-                $.session.set('DeptID', data.DeptID);
-            })
-            .error(function () {
-                $scope.loginerrormessage = true;
-            })
-        }
-        else {
-            $scope.message = true;
-        }
     }
 }
