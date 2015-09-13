b9ec3958261faea66849db3e071b0734e6924c13
a0134741 <a0134741@u.nus.edu>
2015-09-13 18:26:44 +0800

    Updae

---
+++ Web/js/login.js
@@ -0,0 +1,14 @@
+﻿var myApp = angular.module('login', []);
+
+myApp.controller('logincontroller', ['$scope',logincontroller]);
+function logincontroller($scope) {
+    $scope.spice = 'very';
+
+    $scope.chiliSpicy = function () {
+        $scope.spice = 'chili';
+    };
+
+    $scope.jalapenoSpicy = function () {
+        $scope.spice = 'jalapeño';
+    };
+}
