557722600fc785e2de2bc08980bb23c25b711579
a0070007 <a0070007@u.nus.edu>
2015-09-14 21:02:11 +0800

    update reqCtrl

--- Web/js/controller/RequisitionController.js
+++ Web/js/controller/RequisitionController.js
@@ -14,7 +14,7 @@
             var status = $rootScope.optiondata.selectedOption.StatusID;
             if (status == 0) { status = "null"; }
             var ReqID = $scope.ReuisitionNo;
-            if (ReqID == null || ReqID == "") { ReqID = "null" }
+            if (ReqID == null || ReqID == "") { ReqID = "null"; }
             BaseService.getRequisitionList(status, ReqID, $rootScope.UserInfo.EmpId, $rootScope.UserInfo.DeptId)
                 .then(function (data) {
                     console.log(data);
