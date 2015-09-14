ac28e7e15ea73f611dc66dbe52db901ec1a5f92a
a0134741 <a0134741@u.nus.edu>
2015-09-14 18:09:06 +0800

    Updage delegate

--- Web/js/controller/DelegateController.js
+++ Web/js/controller/DelegateController.js
@@ -23,6 +23,20 @@
             BaseService.removeDelegate(delegagte.DelegateSN)
                 .then(function (data) {
                     alert("Success Delect!");
+                    BaseService.getDeptDelegate($rootScope.UserInfo.DeptId)
+            .then(function (data) {
+                console.log(data);
+                $scope.Delegates = data;
+                $.each(data, function (index, value) {
+                    if (value.EmpID != null) {
+                        MyBaseService.getEmployee(value.EmpID)
+                            .then(function (data) {
+                                value.EmpName = data.EmpName;
+                            }
+                            )
+                    }
+                })
+            })
                 })
         }
         $scope.newDelegate = function () {
