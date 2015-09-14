ea6ab5870f14fe9b3bb27c6a8c2ebd6a8000af08
a0070007 <a0070007@u.nus.edu>
2015-09-14 12:03:23 +0800

    build

--- RestService/IRequisitionAPI.cs
+++ RestService/IRequisitionAPI.cs
@@ -105,6 +105,6 @@ namespace RestService
         [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
                            BodyStyle = WebMessageBodyStyle.Bare,
                            UriTemplate = "/getRequisitionApprovalList/{DeptID}")]
-        List<Requisition> getRequisitionApprovalList(string DeptID)
+        List<Requisition> getRequisitionApprovalList(string DeptID);
     }
 }
