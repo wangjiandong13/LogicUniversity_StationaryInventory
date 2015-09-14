6dc17aef21644850c824c105e4d6bd1e4802c0f4
a0134741 <a0134741@u.nus.edu>
2015-09-14 11:02:50 +0800

    Updae

--- Web/js/controller/DelegateController.js
+++ Web/js/controller/DelegateController.js
@@ -2,7 +2,8 @@
     app.controller('DelegateControllers', ['$rootScope', '$scope', 'BaseService', DelegateControllers]);
 
     function DelegateControllers($rootScope, $scope, BaseService) {
-
+        //sidebar highlight
+        $rootScope.changehighlight(1);
     }
 
 })
