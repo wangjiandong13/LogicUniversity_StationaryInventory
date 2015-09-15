1935ef391a4249204623cc71e75d2354e760c371
a0135523 <a0135523@u.nus.edu>
2015-09-15 14:21:53 +0800

    update on JS

--- Web/js/controller/SupplierdetailController.js
+++ Web/js/controller/SupplierdetailController.js
@@ -1,1 +1,6 @@
-﻿
+﻿define(['app'], function (app) {
+    app.controller('SupplierdetailController', ['$rootScope', '$scope', 'BaseService', SupplierdetailController]);
+    function SupplierdetailController($rootScope, $scope, BaseService) {
+        $rootScope.changehighlight(16);
+    }
+});
