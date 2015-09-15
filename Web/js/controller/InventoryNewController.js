69f341f26131abfee7ebbb79ebc8eca373397915
a0070007 <a0070007@u.nus.edu>
2015-09-15 09:45:21 +0800

    update inventoryNewCtrl

--- Web/js/controller/InventoryNewController.js
+++ Web/js/controller/InventoryNewController.js
@@ -6,18 +6,20 @@
         //set mean highlight
         $rootScope.changehighlight(12);
 
+        var itemid = $routeParams.itemid;
+        $scope.itemid = itemid;
         var myBaseService = BaseService;
         
         //get categoryselectData
         $scope.CategorySelectData = {
             availableOptions: [],
-            selectedOption: { ItemCatID: 0, ItemDescription: 'ALL' }
+            selectedOption: { ItemCatID: 1, ItemDescription: 'Clips' }
         };
         BaseService.getCategory()
             .then(function (data) {
                 //console.log(data);
                 $scope.CategorySelectData.availableOptions = data;
-                $scope.CategorySelectData.availableOptions.unshift({ ItemCatID: 0, ItemDescription: 'ALL' });
+                //$scope.CategorySelectData.availableOptions.unshift({ ItemCatID: 0, ItemDescription: 'ALL' });
             }, function (data) {
                 alert(data);
             })
@@ -52,36 +54,56 @@
         else {
             $scope.Title = "Edit";
 
+            //load item data
+            BaseService.getItemDetail(itemid)
+            .then(function (data) {
+                console.log(data);
+                $scope.ItemDesc = data.ItemName;
+                $scope.ItemID = data.ItemID;
+                $scope.CategorySelectData.selectedOption.ItemCatID = data.ItemCatID;
+                $scope.CategorySelectData.selectedOption.ItemDescription = data.ItemDescription;
+                $scope.RoLvl = data.RoLvl;
+                $scope.RoQty = data.RoQty;
+                $scope.UOM = data.UOM;
+            }, function (data) {
+                alert(data);
+            })
+            //load itemPrice data
+            BaseService.getItemPrice(itemid)
+            .then(function (data) {
+                console.log(data);
+                $scope.supplier1 = data[0].SupplierID;
+                $scope.supplier1Price = data[0].Price;
+                $scope.supplier2 = data[1].SupplierID;
+                $scope.supplier2Price = data[1].Price;
+                $scope.supplier3 = data[2].SupplierID;
+                $scope.supplier3Price = data[2].Price;
+            }, function (data) {
+                alert(data);
+            })
+
             //Edit: click submit 
             $scope.submit = function () {
                 console.log("enter");
-                var msg = [];
-                if ($rootScope.RequestCarts != null) {
-                    $.each($rootScope.RequestCarts, function (index, value) {
-                        var each = {
-                            EmpID: $rootScope.UserInfo.EmpId,
-                            ItemID: value.ItemID,
-                            Qty: value.Qty,
+                var msgItem = {
+                    ItemID: $scope.ItemID, ItemName: $scope.ItemDesc, ItemCatID: $scope.CategorySelectData.selectedOption.ItemCatID,
+                    RoLvl: $scope.RoLvl, RoQty: $scope.RoQty, UOM: $scope.UOM
+                };
+                var msgItemPrice = [{ ItemID: $scope.ItemID, SupplierID: $scope.supplier1, Price: $scope.supplier1Price },
+                            { ItemID: $scope.ItemID, SupplierID: $scope.supplier2, Price: $scope.supplier2Price },
+                            { ItemID: $scope.ItemID, SupplierID: $scope.supplier3, Price: $scope.supplier3Price }];
 
-                        };
-                        msg.push(each);
-                    });
-                    var req_id = "";
-                    selfBaseService.createRequisition(angular.toJson(msg))
-                        .then(function (data) {
-                            console.log(data);
-                            req_id = data;
-                            var priority = 2;
-                            if ($('#create-switch').is(":checked")) { priority = 1 }
-                            selfBaseService.setReqPriority(req_id, priority, $scope.remoarks)
-                                .then(function (data) {
-                                    alert("success!");
-                                    location.href = '#/requisition';
-                                })
-                        }, function (data) {
-                            alert(data);
-                        })
-                }
+                myBaseService.updateItemInv(angular.toJson(msgItem))
+                    .then(function (data) {
+                        console.log(data);
+                        myBaseService.updateItemPriceInv(angular.toJson(msgItemPrice))
+                            .then(function (data) {
+                                alert("success!");
+                                location.href = '#/inventoryList';
+                            })
+                    }, function (data) {
+                        alert(data);
+                    })
             }
         }
 
