7d3ca8a0479e3b23e9eeb498fe86c53152faa7c6
a0134741 <a0134741@u.nus.edu>
2015-09-15 10:52:37 +0800

    Updaye

--- Web/js/controller/InventoryListController.js
+++ Web/js/controller/InventoryListController.js
@@ -36,8 +36,8 @@
             if (S_categoryname == "" || S_categoryname == null) {
                 S_categoryname = "null";
             }
-            //console.log(S_category);
-            //console.log(S_categoryname);
+            console.log("S_category"+S_category);
+            console.log("S_categoryname"+S_categoryname);
             BaseService.searchItem(S_category, S_categoryname)
                 .then(function (data) {
                     //console.log(data);
@@ -45,12 +45,12 @@
                     $.each($scope.inventoryListdata, function (index, value) {
                         myBaseService.getItemPrice(value.ItemID)
                             .then(function (itemdata) {
-                                //console.log(itemdata);
-                                //console.log($scope.supplierID);
+                                console.log("itemdata"+itemdata);
+                                console.log("$scope.supplierID"+$scope.supplierID);
                                 $.each(itemdata, function (index, ipvalue) {
-                                    //console.log(ipvalue.SupplierID);
+                                    console.log(ipvalue.SupplierID);
                                     if (ipvalue.SupplierID == $scope.supplierID) {
-                                        //console.log($scope.inventoryListdata.Price);
+                                        console.log($scope.inventoryListdata.Price);
                                         value.Price = ipvalue.Price;
                                     }
 
