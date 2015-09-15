bb57dd78c771850f91a3f733f3a51365ce308115
a0134741 <a0134741@u.nus.edu>
2015-09-15 12:35:59 +0800

    UPdate

--- Web/js/controller/PurchaseOrderDetailController.js
+++ Web/js/controller/PurchaseOrderDetailController.js
@@ -1,6 +1,9 @@
 ﻿define(['app'], function (app) {
     app.controller('PurchaseOrderDetailControllers', ['$rootScope', '$scope', 'BaseService', PurchaseOrderDetailControllers]);
     function PurchaseOrderDetailControllers($rootScope, $scope, BaseService) {
+        $rootScope.changehighlight(13);
 
+        var PoID = $routeParams.PoID;
+        alert(PoID);
     }
 })
