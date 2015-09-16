40b9596d268daf8b7732f12ac06b18494a6b6cdf
a0134741 <a0134741@u.nus.edu>
2015-09-16 17:53:50 +0800

    Update

--- Web/js/controller/AnalyticsDetailController.js
+++ Web/js/controller/AnalyticsDetailController.js
@@ -1,10 +1,10 @@
 ﻿define(['app'], function (app) {
-    app.controller('AnalyticsDetailController', ['$scope', '$rootScope', "$routeParams", 'BaseService', AnalyticsDetailController]);
+    app.controller('AnalyticsDetailController', ['$scope', '$rootScope', 'BaseService', AnalyticsDetailController]);
     app.controller('Chatcontroller', ['$scope', '$rootScope', 'BaseService', Chatcontroller]);
-    function AnalyticsDetailController($scope, $rootScope, $routeParams, BaseService) {
+    function AnalyticsDetailController($scope, $rootScope, BaseService) {
 
     }
-    function Chatcontroller($scope) {
+    function Chatcontroller($scope, $rootScope, BaseService) {
         $scope.options = {
             chart: {
                 type: 'multiBarChart',
@@ -36,50 +36,56 @@
             }
         };
 
-        $scope.data = generateData();
+        $scope.data = 
 
-        /* Random Data Generator (took from nvd3.org) */
+
+
         function generateData() {
-            return stream_layers(3, 50 + Math.random() * 50, .1).map(function (data, i) {
-                return {
-                    key: 'Stream' + i,
-                    values: data
-                };
-            });
+            BaseService.
         }
 
-        /* Inspired by Lee Byron's test data generator. */
-        function stream_layers(n, m, o) {
-            if (arguments.length < 3) o = 0;
-            function bump(a) {
-                var x = 1 / (.1 + Math.random()),
-                    y = 2 * Math.random() - .5,
-                    z = 10 / (.1 + Math.random());
-                for (var i = 0; i < m; i++) {
-                    var w = (i / m - y) * z;
-                    a[i] += x * Math.exp(-w * w);
-                }
-            }
-            return d3.range(n).map(function () {
-                var a = [], i;
-                for (i = 0; i < m; i++) a[i] = o + o * Math.random();
-                for (i = 0; i < 5; i++) bump(a);
-                return a.map(stream_index);
-            });
-        }
+        ///* Random Data Generator (took from nvd3.org) */
+        //function generateData() {
+        //    return stream_layers(3, 50 + Math.random() * 50, .1).map(function (data, i) {
+        //        return {
+        //            key: 'Stream' + i,
+        //            values: data
+        //        };
+        //    });
+        //}
 
-        /* Another layer generator using gamma distributions. */
-        function stream_waves(n, m) {
-            return d3.range(n).map(function (i) {
-                return d3.range(m).map(function (j) {
-                    var x = 20 * j / m - i / 3;
-                    return 2 * x * Math.exp(-.5 * x);
-                }).map(stream_index);
-            });
-        }
+        ///* Inspired by Lee Byron's test data generator. */
+        //function stream_layers(n, m, o) {
+        //    if (arguments.length < 3) o = 0;
+        //    function bump(a) {
+        //        var x = 1 / (.1 + Math.random()),
+        //            y = 2 * Math.random() - .5,
+        //            z = 10 / (.1 + Math.random());
+        //        for (var i = 0; i < m; i++) {
+        //            var w = (i / m - y) * z;
+        //            a[i] += x * Math.exp(-w * w);
+        //        }
+        //    }
+        //    return d3.range(n).map(function () {
+        //        var a = [], i;
+        //        for (i = 0; i < m; i++) a[i] = o + o * Math.random();
+        //        for (i = 0; i < 5; i++) bump(a);
+        //        return a.map(stream_index);
+        //    });
+        //}
 
-        function stream_index(d, i) {
-            return { x: i, y: Math.max(0, d) };
-        }
+        ///* Another layer generator using gamma distributions. */
+        //function stream_waves(n, m) {
+        //    return d3.range(n).map(function (i) {
+        //        return d3.range(m).map(function (j) {
+        //            var x = 20 * j / m - i / 3;
+        //            return 2 * x * Math.exp(-.5 * x);
+        //        }).map(stream_index);
+        //    });
+        //}
+
+        //function stream_index(d, i) {
+        //    return { x: i, y: Math.max(0, d) };
+        //}
     }
 })
