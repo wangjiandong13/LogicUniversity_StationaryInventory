0bd9b37b659530a029766c558fda69ba84e4d686
a0070007 <a0070007@u.nus.edu>
2015-09-16 16:23:06 +0800

    update retrievalDetailDept

--- Web/js/controller/RetrievalDetailDeptController.js
+++ Web/js/controller/RetrievalDetailDeptController.js
@@ -1,1 +1,53 @@
-﻿
+﻿define(['app'], function (app) {
+    app.controller('RetrievalDetailDeptControllers', ['$rootScope', '$scope', 'BaseService', '$routeParams', RetrievalDetailDeptControllers]);
+
+    function RetrievalDetailDeptControllers($rootScope, $scope, BaseService, $routeParams) {
+        var RetID = 1017;
+        $rootScope.changehighlight(14);
+
+        var retid = $routeParams.retid;
+        $scope.retid = retid;
+        console.log($scope.retid);
+
+        var myBaseService = BaseService;
+
+        BaseService.getRetrievalListBySC("null", "null", $scope.retid)
+            .then(function (data) {
+                console.log(data);
+                $scope.RetrievalData = data[0];
+
+            }, function (data) {
+                alert(data);
+            })
+
+        BaseService.getRequisitionListByRetID($scope.retid)
+            .then(function (data) {
+                console.log(data);
+                $scope.RequisitionData = data;
+            }, function (data) {
+                alert(data);
+            })
+
+        BaseService.getRetrievalDetail(RetID)
+            .then(function (data) {
+                console.log(data);
+                $scope.RetrievalDetail = data;
+            }, function (data) {
+                alert(data);
+            })
+        BaseService.getReqAllocation(RetID)
+            .then(function (data) {
+                console.log(data);
+                $scope.ReqAllocation = data;
+            }, function (data) {
+                alert(data);
+            })
+
+        $scope.viewByReq = function () {
+            location.href = "#/retrievalDetailReq/" + retid;
+        }
+        $scope.backToList = function () {
+            location.href = "#/retrieval";
+        }
+    }
+})
