caf280b25c5ef2315a637ac31872d3e3a8fdf049
a0134741 <a0134741@u.nus.edu>
2015-09-17 00:38:30 +0800

    Update

--- Web/js/controller/AnalyticsDetailController.js
+++ Web/js/controller/AnalyticsDetailController.js
@@ -1,22 +1,8 @@
 ﻿define(['app'], function (app) {
-    app.controller('AnalyticsDetailController', ['$scope', '$rootScope', 'BaseService', AnalyticsDetailController]);
-    app.controller('Chatcontroller', ['$scope', '$rootScope', 'BaseService', Chatcontroller]);
+    app.controller('AnalyticsDetailController', ['$scope', '$rootScope', '$routeParams', 'BaseService', AnalyticsDetailController]);
     function AnalyticsDetailController($scope, $rootScope, BaseService) {
-
-    }
-    function Chatcontroller($scope, $rootScope, BaseService) {
-        var Pricedata = [];
-        var Qtydata = [];
-        var each = {
-            key: "",
-            values: []
-        };
-        var test = [];
-        var eachvalue = {
-            x: "",
-            y: 0
-        };
-        $scope.options = {
+        var rid = $routeParams.reportid;
+        $scope.Qtyoptions = {
             chart: {
                 type: 'multiBarChart',
                 height: 450,
@@ -46,25 +32,69 @@
                 }
             }
         };
-        //$scope.data = [{
-        //    key: "红笔",
-        //    values: [{ x: "Jan 2015", y: 10 }, { x: "Feb 2015", y: 25 }]
-        //}, {
-        //    key: "蓝笔",
-        //    values: [{ x: "Jan 2015", y: 15 }, { x: "Feb 2015", y: 500 }]
-        //}];
-        BaseService.generateExistingReport(1,"Qty")
+        BaseService.generateExistingReport(rid, "Qty")
             .then(function (data) {
-                console.log(data);
+                //console.log(data);
                 //generateQtyData(data)
-                $scope.data = data;
-                console.log($scope.data);
+                $scope.Qtydata = data;
+                //console.log($scope.data);
             })
+        $scope.Priceoptions = {
+            chart: {
+                type: 'multiBarChart',
+                height: 450,
+                margin: {
+                    top: 20,
+                    right: 20,
+                    bottom: 60,
+                    left: 45
+                },
+                clipEdge: true,
+                staggerLabels: true,
+                transitionDuration: 500,
+                stacked: true,
+                xAxis: {
+                    axisLabel: 'Time (Month)',
+                    showMaxMin: false,
+                    tickFormat: function (d) {
+                        return d;
+                    }
+                },
+                yAxis: {
+                    axisLabel: 'Y Axis',
+                    axisLabelDistance: 40,
+                    tickFormat: function (d) {
+                        return d3.format(',.1f')(d);
+                    }
+                }
+            }
+        };
+        BaseService.generateExistingReport(rid, "Price")
+            .then(function (data) {
+                //console.log(data);
+                //generateQtyData(data)
+                $scope.Pricedata = data;
+                //console.log($scope.data);
+            })
+    }
+    function Chatcontroller($scope, $rootScope,$routeParams, BaseService) {
+        //var Pricedata = [];
+        //var Qtydata = [];
+        //var each = {
+        //    key: "",
+        //    values: []
+        //};
+        //var test = [];
+        //var eachvalue = {
+        //    x: "",
+        //    y: 0
+        //};
+        
         //function generateQtyData(data) {
         //    $.each(data, function (index, value) {
         //        each.key = value.MonthYear;
-        //        console.log("value.ReportItems");
-        //        console.log(value.ReportItems);
+        //        //console.log("value.ReportItems");
+        //        //console.log(value.ReportItems);
         //        $.each(value.ReportItems, function (index, value) {
         //            //eachvalue.x = value.Subject;
         //            //eachvalue.y = value.Qty;
@@ -72,20 +102,20 @@
         //                x: value.Subject,
         //                y: value.Qty
         //            };
-        //            console.log(">>>> enter each");
-        //            console.log(eachvalue1);
+        //            //console.log(">>>> enter each");
+        //            //console.log(eachvalue1);
         //            test.push(eachvalue1);
-        //            console.log(">>>> enter each.values");
-        //            console.log(test);
+        //            //console.log(">>>> enter each.values");
+        //            //console.log(test);
         //        });
         //        //Qtydata.push(each);
-        //        //console.log(each);
+        //        ////console.log(each);
         //    })
-        //    console.log(Qtydata);
+        //    //console.log(Qtydata);
         //}
         //function generatePriceData(data) {
-        //    console.log(">>>>>>Pricedata");
-        //    console.log(Pricedata);
+        //    //console.log(">>>>>>Pricedata");
+        //    //console.log(Pricedata);
         //    $.each(data, function (index, value) {
         //        each.key = value.MonthYear;
         //        $.each(value.ReportItems, function (index, value) {
@@ -94,12 +124,12 @@
         //            each.values.push(eachvalue);
         //        });
         //        Pricedata.push(each);
-        //        console.log(">>>>>>each");
-        //        console.log(each);
-        //        console.log(">>>>>>Pricedata");
-        //        console.log(Pricedata);
+        //        //console.log(">>>>>>each");
+        //        //console.log(each);
+        //        //console.log(">>>>>>Pricedata");
+        //        //console.log(Pricedata);
         //    })
-        //    console.log(Pricedata);
+        //    //console.log(Pricedata);
         //}
     }})
        
