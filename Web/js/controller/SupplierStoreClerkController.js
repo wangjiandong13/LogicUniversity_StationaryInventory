8f35c7a399d7889c81a0aa4e31120b4b4b962d92
a0070007 <a0070007@u.nus.edu>
2015-09-16 10:44:53 +0800

    remove supplierSC and supplierDetail ctrl and html

--- Web/js/controller/SupplierStoreClerkController.js
+++
@@ -1,6 +0,0 @@
-﻿define(['app'], function (app) {
-    app.controller('SupplierStoreClerkController', ['$rootScope', '$scope', 'BaseService', SupplierStoreClerkController]);
-    function SupplierStoreClerkController($rootScope, $scope, BaseService) {
-        $rootScope.changehighlight(15);
-    }
-});
