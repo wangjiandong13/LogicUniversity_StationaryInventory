b5d193918fcd56f8589669d35e00820832f8560e
a0135523 <a0135523@u.nus.edu>
2015-09-18 15:47:51 +0800

    update purchaseOrderController

--- BusinessLogic/PurchaseOrderController.cs
+++ BusinessLogic/PurchaseOrderController.cs
@@ -94,23 +94,18 @@ namespace BusinessLogic
 
                 poDetailSearch.ActualQty = poDetail.ActualQty;
 
-                //update stock card
-                List<StockCard> stockCardList = ctx.StockCard.Where(x => x.ItemID == poDetail.ItemID).ToList();
-                int balance = 0;
-                if (stockCardList.FirstOrDefault() != null)
-                    balance = (int)stockCardList.Last().Balance;
+                //update stock in item
+                Item item = ctx.Item.Where(x => x.ItemID == poDetail.ItemID).FirstOrDefault();
+                item.Stock += poDetail.ActualQty;
 
+                //update stock card
                 StockCard stockCard = new StockCard();
                 stockCard.ItemID = poDetail.ItemID;
                 stockCard.Date = DateTime.Now;
                 stockCard.Description = "Supplier - " + po.SupplierID;
                 stockCard.Qty = poDetail.ActualQty;
-                stockCard.Balance = balance + poDetail.ActualQty;
+                stockCard.Balance = item.Stock + poDetail.ActualQty;
                 ctx.StockCard.Add(stockCard);
-
-                //update stock in item
-                Item item = ctx.Item.Where(x => x.ItemID == poDetail.ItemID).FirstOrDefault();
-                item.Stock = balance + poDetail.ActualQty;
             }
 
             //change status of purchase order to "Delivered"
